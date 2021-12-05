0043fb73c9e3657157fc8c554d53ee52e8e2c3bc
Zoya Volkova <zoearkhipova@gmail.com>
2021-12-05 19:38:37 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from './components/Form.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Form,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+
+  box-sizing: border-box;
+
+  width: 1200px;
+
+  margin: 30px auto;
+}
+
+html,
+body,
+#app {
+  height: 100%;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
